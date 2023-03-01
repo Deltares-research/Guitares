3723a8f4dcee2dd0e5058963b38b7d4bec04e6e8
Frederique <frederique.degroen@deltares.nl>
2023-03-01 11:56:22 +0100

    Added copyright and license to scripts.

--- app/operations/ra2ce/run_manager.py
+++ app/operations/ra2ce/run_manager.py
@@ -1,4 +1,11 @@
-# -*- coding: utf-8 -*-
+# © Deltares 2023.
+# License notice: This file is part of RA2CE GUI. RA2CE GUI is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version
+# 3 of the License, or (at your option) any later version. RA2CE GUI is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Lesser General Public License for more details. You should have received a copy of the GNU Lesser General
+# Public License along with RA2CE GUI. If not, see <https://www.gnu.org/licenses/>.
+
 from app.ra2ceGUI_base import Ra2ceGUI
 from src.guitools.pyqt5.io import openFolderNameDialog
 
