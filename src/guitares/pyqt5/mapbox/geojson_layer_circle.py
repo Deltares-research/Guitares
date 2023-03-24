4651fa54eedc30475fbed3b286be64361cbbbcb0
Panos <panosatha90@gmail.com>
2023-03-24 13:15:27 +0100

    added hover popup option in circle layer

--- src/guitares/pyqt5/mapbox/geojson_layer_circle.py
+++ src/guitares/pyqt5/mapbox/geojson_layer_circle.py
@@ -15,8 +15,8 @@ class GeoJSONLayerCircle(Layer):
 
     def set_data(self,
                  data,
-                 legend_title="",
-                 crs=None):
+                 hover_property = ""
+                 ):
 
         # Make sure this is not an empty GeoDataFrame
         if isinstance(data, GeoDataFrame):
@@ -30,9 +30,9 @@ class GeoJSONLayerCircle(Layer):
         # Add new layer        
         self.mapbox.runjs("./js/geojson_layer_circle.js", "addLayer", arglist=[self.map_id,
                                                                                data.to_crs(4326),
+                                                                               hover_property,
                                                                                self.line_color,
                                                                                self.line_width,
-                                                                               self.line_style,
                                                                                self.line_opacity,
                                                                                self.fill_color,
                                                                                self.fill_opacity,
@@ -42,7 +42,6 @@ class GeoJSONLayerCircle(Layer):
         self.mapbox.runjs("./js/geojson_layer_circle.js", "setPaintProperties", arglist=[self.map_id,
                                                                                          self.line_color,
                                                                                          self.line_width,
-                                                                                         self.line_style,
                                                                                          self.line_opacity,
                                                                                          self.fill_color,
                                                                                          self.fill_opacity,
@@ -52,41 +51,13 @@ class GeoJSONLayerCircle(Layer):
         self.mapbox.runjs("./js/geojson_layer_circle.js", "setPaintProperties", arglist=[self.map_id,
                                                                                          self.line_color_inactive,
                                                                                          self.line_width_inactive,
-                                                                                         self.line_style_inactive,
                                                                                          self.line_opacity_inactive,
                                                                                          self.fill_color_inactive,
                                                                                          self.fill_opacity_inactive,
                                                                                          self.circle_radius_inactive])
 
-    # def activate(self):
-    #     self.active = True
-
-    # def deactivate(self):
-    #     self.active = False
-
-    # def clear(self):
-    #     self.active = False
-    #     self.mapbox.runjs("/js/main.js", "removeLayer", arglist=[self.map_id])
-
-    # def update(self):
-    #     pass
-
-    # def set_data(self,
-    #              data,
-    #              legend_title="",
-    #              crs=None):
-    #     # Make sure this is not an empty GeoDataFrame
-    #     if isinstance(data, GeoDataFrame):
-    #         # Data is GeoDataFrame
-    #         if len(data) > 0:
-    #             # Convert GDF to geojson
-    #             data = data.to_json()
-    #             self.mapbox.runjs("/js/geojson_layer.js", "setData", arglist=[self.map_id, data])
-
     def set_visibility(self, true_or_false):
         if true_or_false:
-            self.mapbox.runjs("/js/main.js", "showLayer", arglist=[self.map_id + ".fill"])
-            self.mapbox.runjs("/js/main.js", "showLayer", arglist=[self.map_id + ".line"])
+            self.mapbox.runjs("/js/main.js", "showLayer", arglist=[self.map_id])
         else:
-            self.mapbox.runjs("/js/main.js", "hideLayer", arglist=[self.map_id + ".fill"])
-            self.mapbox.runjs("/js/main.js", "hideLayer", arglist=[self.map_id + ".line"])
+            self.mapbox.runjs("/js/main.js", "hideLayer", arglist=[self.map_id])
--- src/guitares/pyqt5/mapbox/server/js/geojson_layer_circle.js
+++ src/guitares/pyqt5/mapbox/server/js/geojson_layer_circle.js
@@ -1,7 +1,14 @@
-import { map, featureClicked, mapboxgl } from '/js/main.js';
+import { map , mapboxgl } from '/js/main.js';
 
-export function addLayer(id, data, lineColor, lineWidth, lineStyle, lineOpacity, fillColor, fillOpacity, circleRadius) {
-
+export function addLayer(id, data, hover_property,
+  lineColor, 
+  lineWidth, 
+  lineOpacity, 
+  fillColor, 
+  fillOpacity, 
+  circleRadius) 
+  
+  {
   map.addSource(id, {
     type: 'geojson',
     data: data
@@ -16,10 +23,45 @@ export function addLayer(id, data, lineColor, lineWidth, lineStyle, lineOpacity,
       'circle-color': fillColor,
       'circle-stroke-width': lineWidth,
       'circle-stroke-color': lineColor,
+      'circle-stroke-opacity': lineOpacity,
       'circle-radius': circleRadius,
-      'circle-opacity': 0.75
+      'circle-opacity': fillOpacity
     }
   });
+
+  if (hover_property !== ""){
+
+  // Create a popup, but don't add it to the map yet.
+  const popup = new mapboxgl.Popup({
+    closeButton: false,
+    closeOnClick: false
+    });
+
+  map.on('mouseenter', id, (e) => {
+    // Change the cursor style as a UI indicator.
+    map.getCanvas().style.cursor = 'pointer';
+     
+    // Copy coordinates array.
+    const coordinates = e.features[0].geometry.coordinates.slice();
+     
+    // Ensure that if the map is zoomed out such that multiple
+    // copies of the feature are visible, the popup appears
+    // over the copy being pointed to.
+    while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+    coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+    }
+     
+    // Display a popup 
+    popup.setLngLat(e.lngLat)
+    .setText(hover_property + ": " + e.features[0].properties[hover_property])
+    .addTo(map);
+    });
+     
+    map.on('mouseleave', id, () => {
+    map.getCanvas().style.cursor = '';
+    popup.remove();
+    });
+  }
 };
 
 export function setData(id, data) {
@@ -32,7 +74,6 @@ export function setData(id, data) {
 export function setPaintProperties(id,
   lineColor,
   lineWidth,
-  lineStyle,
   lineOpacity,
   fillColor,
   fillOpacity,                         
@@ -40,7 +81,10 @@ export function setPaintProperties(id,
 
   if (map.getLayer(id)) {  
     map.setPaintProperty(id, 'circle-stroke-color', lineColor);
-    map.setPaintProperty(id, 'circle-color', fillColor);                  
+    map.setPaintProperty(id, 'circle-stroke-width', lineWidth);
+    map.setPaintProperty(id, 'circle-stroke-opacity', lineOpacity);
+    map.setPaintProperty(id, 'circle-color', fillColor);   
+    map.setPaintProperty(id, 'circle-opacity', fillOpacity);                
     map.setPaintProperty(id, 'circle-radius', circleRadius);  
   }
 }
