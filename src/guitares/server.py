1386bc1231b5cb0bcd5263300657524811c37757
maartenvanormondt <maarten.vanormondt@deltares.nl>
2023-06-13 12:04:16 -0400

    added some exceptions to server and option to use node.js server

--- src/guitares/server.py
+++ src/guitares/server.py
@@ -4,6 +4,41 @@ import os
 import time
 import urllib
 
+# Custom Exception Class
+class MyException(Exception):
+    pass
+ 
+# Custom Thread Class
+class ServerThread(threading.Thread):
+    def __init__(self, server_path, server_port):
+        super().__init__(daemon=True)
+        self.server_path = server_path
+        self.server_port = server_port
+     
+  # Function that raises the custom exception
+    def run_server(self):
+        print("Server path : " + self.server_path)
+        httpd = HTTPServer(self.server_path, ("", self.server_port))
+        httpd.serve_forever()
+        name = threading.current_thread().name
+        raise MyException("An error in thread " + name)
+ 
+    def run(self):       
+        # Variable that stores the exception, if raised by run_server
+        self.exc = None           
+        try:
+            self.run_server()
+        except BaseException as e:
+            self.exc = e
+       
+    # def join(self):
+    #     threading.Thread.join(self)
+    #     # Since join() returns in caller thread
+    #     # we re-raise the caught exception
+    #     # if any was caught
+    #     if self.exc:
+    #         raise self.exc
+        
 class HTTPHandler(SimpleHTTPRequestHandler):
     """This handler uses server.base_path instead of always using os.getcwd()"""
     def translate_path(self, path):
@@ -18,24 +53,52 @@ class HTTPServer(BaseHTTPServer):
         self.base_path = base_path
         BaseHTTPServer.__init__(self, server_address, RequestHandlerClass)
 
-def run_server(server_path, server_port):
-    print("Server path : " + server_path)
-    httpd = HTTPServer(server_path, ("", server_port))
-    httpd.serve_forever()
+# def run_server(server_path, server_port):
+#     print("Server path : " + server_path)
+#     httpd = HTTPServer(server_path, ("", server_port))
+#     httpd.serve_forever()
 
-def start_server(server_path, port=3000):
-    thr = threading.Thread(target=run_server, args=(server_path, port), daemon=True)
-    thr.start()
-    while True:
-        try:        
-            print("Finding server ...")        
-            urllib.request.urlcleanup()
-            url = "http://localhost:" + str(port)
-            request = urllib.request.urlopen(url)
-            request.close()
-            print("Found server running at port " + str(port) + " ...")        
-            break        
-        except:
-            print("Waiting for server ...")
-            time.sleep(1)
-    return thr       
+def run_node_server(server_path, server_port):
+    print("Node server path : " + server_path)
+    os.chdir(server_path)
+    os.system("run_node_server.bat")
+
+def start_server(server_path, port=3000, node=False):
+    if node:
+        thr = threading.Thread(target=run_node_server, args=(server_path, port), daemon=True)
+        thr.start()
+        while True:
+            try:        
+                print("Finding server ...")        
+                urllib.request.urlcleanup()
+                url = "http://localhost:" + str(port)
+                request = urllib.request.urlopen(url)
+                request.close()
+                print("Found server running at port " + str(port) + " ...")        
+                break        
+            except:
+                print("Waiting for server ...")
+                time.sleep(1)
+        return thr       
+    else:
+#        thr = threading.Thread(target=run_server, args=(server_path, port), daemon=True)
+        thr = ServerThread(server_path, port)
+        thr.start()
+        # # Exception handled in Caller thread
+        # try:
+        #     thr.join()
+        # except Exception as e:
+        #     print("Exception Handled in Main, Details of the Exception:", e)
+        while True:
+            try:        
+                print("Finding server ...")        
+                urllib.request.urlcleanup()
+                url = "http://localhost:" + str(port)
+                request = urllib.request.urlopen(url)
+                request.close()
+                print("Found server running at port " + str(port) + " ...")        
+                break        
+            except:
+                print("Waiting for server ...")
+                time.sleep(1)
+        return thr       
