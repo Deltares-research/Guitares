eb3a451e02fc0f9b58226a6f7e15b88120e9017f
Panos Athanasiou <Panos.Athanasiou@deltares.nl>
2023-10-13 17:19:25 +0200

    add functionality to have option as variable in radiobuttons

--- src/guitares/pyqt5/radiobuttongroup.py
+++ src/guitares/pyqt5/radiobuttongroup.py
@@ -1,5 +1,4 @@
 from PyQt5.QtWidgets import QButtonGroup, QRadioButton
-from PyQt5 import QtCore
 import traceback
 
 class RadioButtonGroup(QButtonGroup):
@@ -9,8 +8,16 @@ class RadioButtonGroup(QButtonGroup):
 
         self.element = element
 
-        for i in range(len(element.option_value.list)):
-            d = QRadioButton(element.option_string.list[i], element.parent.widget)
+        if element.option_value.variable:
+            group = element.option_value.variable_group
+            name  = element.option_value.variable
+            self.element.option_value.list = element.getvar(group, name)
+        if element.option_string.variable:
+            group = element.option_string.variable_group
+            name  = element.option_string.variable
+            self.element.option_string.list = element.getvar(group, name)
+        for i in range(len(self.element.option_value.list)):
+            d = QRadioButton(self.element.option_string.list[i], self.element.parent.widget)
             d.setVisible(True)
             d.id = i
             self.addButton(d)
@@ -66,3 +73,21 @@ class RadioButtonGroup(QButtonGroup):
         nbuttons = len(self.element.option_value.list)
         for i in range(nbuttons):
             self.buttons()[i].setVisible(true_or_false)
+
+
+    def add_items(self):
+
+        # Delete existing items
+        self.clear()
+
+        if self.element.option_string.variable:
+            group = self.element.option_string.variable_group
+            name  = self.element.option_string.variable
+            v     = self.element.getvar(group, name)
+            if not v:
+                v = [""]
+            for itxt, txt in enumerate(v):
+                self.insertItem(itxt, txt)
+        else:
+            for itxt, txt in enumerate(self.element.option_string.list):
+                self.insertItem(itxt, txt)
